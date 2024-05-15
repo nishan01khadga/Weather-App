a97216fcbad14fa7a91735114cf53b21abc603eb
nishan01khadga <nishan01khadga@gmail.com>
2024-05-15 12:37:09 +0545

    first push

---
+++ lib/weather_bloc/weather_state.dart
@@ -0,0 +1,6 @@
+abstract class WeatherState{}
+
+class WeatherInitialState extends WeatherState{}
+class WeatherLoadingState extends WeatherState{}
+class WeatherLoadedState extends WeatherState{}
+class WeatherLoadingFailureState extends WeatherState{}
